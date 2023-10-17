f2d31ce2de3cfe8c3d0b7c801c1a675a21ae0e7d
Kirill Svitsov <user@MacBook-Air-2.Home>
2023-10-17 16:17:27 +0100

    Changed CSRF_TRUSTED_ORIGINS to https

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -15,7 +15,7 @@ DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1').split()
 
-CSRF_TRUSTED_ORIGINS = ['http://51.250.100.142']
+CSRF_TRUSTED_ORIGINS = ['https://51.250.100.142']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
