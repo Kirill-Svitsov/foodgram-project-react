660668a20304cfec693b672a35eb461c5061829d
Kirill Svitsov <user@MacBook-Air-2.Home>
2023-10-17 17:33:20 +0100

    Try to admin

---
+++ backend/foodgram/import_ingredients.py
@@ -0,0 +1,17 @@
+import json
+
+from recipes.models import Ingredient
+
+
+def load_data_from_json(file_path):
+    with open(file_path) as f:
+        data = json.load(f)
+        for item in data:
+            ingredient = Ingredient(
+                name=item['name'],
+                measurement_unit=item['measurement_unit']
+            )
+            ingredient.save()
+
+
+load_data_from_json('ingredients.json')
