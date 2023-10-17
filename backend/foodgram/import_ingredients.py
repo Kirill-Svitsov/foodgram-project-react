341dfafa6738d169c5e8563c643dd9172b11726f
Kirill Svitsov <user@MacBook-Air-2.Home>
2023-10-17 18:07:39 +0100

    added new CSRF_TRUSTED_ORIGINS ways

--- backend/foodgram/import_ingredients.py
+++ backend/foodgram/import_ingredients.py
@@ -1,17 +1,16 @@
-import json
+import csv
 
 from recipes.models import Ingredient
 
-
-def load_data_from_json(file_path):
-    with open(file_path) as f:
-        data = json.load(f)
-        for item in data:
-            ingredient = Ingredient(
-                name=item['name'],
-                measurement_unit=item['measurement_unit']
-            )
-            ingredient.save()
-
-
-load_data_from_json('ingredients.json')
+with open('data/ingredients.csv', newline='') as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+        name, measurement_unit = row
+        ingredient, created = Ingredient.objects.get_or_create(
+            name=name,
+            measurement_unit=measurement_unit
+        )
+        if created:
+            print(f'Added ingredient: {name} ({measurement_unit})')
+        else:
+            print(f'Ingredient already exists: {name} ({measurement_unit})')
