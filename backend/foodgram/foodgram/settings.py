a99febc0dfdc2b572c0485653b52934a6f3663fa
Kirill Svitsov <user@MacBook-Air-2.Home>
2023-10-18 14:16:10 +0100

    Changed to filters

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -32,7 +32,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
-    'django_filter',
+    'django_filters',
     'djoser',
     'recipes.apps.RecipesConfig',
     'users.apps.UsersConfig',
