4963d3a5966ab37dde6327fa61a49208115b901d
Kirill Svitsov <user@MacBook-Air-2.Home>
2023-10-20 10:46:48 +0100

    Filters again

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -2,40 +2,40 @@ from django_filters.rest_framework import FilterSet, filters
 
 from recipes.models import Ingredient, Recipe, Tag
 
-FILTER_USER = {'favorites': 'favorites__user',
-               'shopping_cart': 'shopping_cart__user'}
 
+class FilterRecipes(FilterSet):
+    """Фильтер для рецептов."""
 
-class IngredientFilter(FilterSet):
-    name = filters.CharFilter(lookup_expr='istartswith')
+    tags = filters.ModelMultipleChoiceFilter(queryset=Tag.objects.all(),
+                                             to_field_name='slug',
+                                             field_name='tags__slug')
 
-    class Meta:
-        model = Ingredient
-        fields = ('name',)
-
-
-class RecipeFilter(FilterSet):
-    tags = filters.ModelMultipleChoiceFilter(
-        field_name='tags__slug',
-        to_field_name='slug',
-        queryset=Tag.objects.all()
-    )
     is_favorited = filters.BooleanFilter(method='filter_is_favorited')
     is_in_shopping_cart = filters.BooleanFilter(
-        method='filter_is_in_shopping_cart'
-    )
+        method='filter_is_in_shopping_cart')
 
-    class Meta:
-        model = Recipe
-        fields = ('tags', 'author', 'is_favorited', 'is_in_shopping_cart')
-
-    def _get_queryset(self, queryset, name, value, model):
-        if value:
-            return queryset.filter(**{FILTER_USER[model]: self.request.user})
+    def filter_is_in_shopping_cart(self, queryset, name, value):
+        """Фильтр для покупок."""
+        if value and self.request.user.is_authenticated:
+            return queryset.filter(shopping_cart__user=self.request.user)
         return queryset
 
     def filter_is_favorited(self, queryset, name, value):
-        return self._get_queryset(queryset, name, value, 'favorites')
+        """Фильтр для избранного."""
+        if value and self.request.user.is_authenticated:
+            return queryset.filter(favorites__user=self.request.user)
+        return queryset
 
-    def filter_is_in_shopping_cart(self, queryset, name, value):
-        return self._get_queryset(queryset, name, value, 'shopping_cart')
+    class Meta:
+        model = Recipe
+        fields = ('author', 'tags')
+
+
+class FilterSearchForName(FilterSet):
+    """Фильтр name."""
+
+    name = filters.CharFilter(lookup_expr='startswith')
+
+    class Meta:
+        model = Ingredient
+        fields = ['name']
