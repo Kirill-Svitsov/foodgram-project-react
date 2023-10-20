4274ceedc17376e769f880ad14f2a85e6582433a
Kirill Svitsov <user@MacBook-Air-2.Home>
2023-10-20 10:31:54 +0100

    Changed filters

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -1,68 +1,41 @@
-from django_filters import rest_framework
-from rest_framework.filters import SearchFilter
+from django_filters.rest_framework import FilterSet, filters
 
 from recipes.models import Ingredient, Recipe, Tag
 
+FILTER_USER = {'favorites': 'favorites__user',
+               'shopping_cart': 'shopping_cart__user'}
 
-class IngredientFilter(SearchFilter):
-    search_param = 'name'
+
+class IngredientFilter(FilterSet):
+    name = filters.CharFilter(lookup_expr='istartswith')
 
     class Meta:
         model = Ingredient
         fields = ('name',)
 
 
-# class RecipeFilter(FilterSet):
-#     tags = filters.ModelMultipleChoiceFilter(
-#         field_name='tags__slug',
-#         to_field_name='slug',
-#         queryset=Tag.objects.all(),
-#     )
-#     is_favorited = filters.NumberFilter(method='filter_is_favorited')
-#     is_in_shopping_cart = filters.NumberFilter(
-#         method='filter_is_in_shopping_cart'
-#     )
-#
-#     class Meta:
-#         model = Recipe
-#         fields = ('tags', 'author', 'is_favorited', 'is_in_shopping_cart',)
-#
-#     def filter_is_favorited(self, queryset, name, value):
-#         if value and self.request.user.is_authenticated:
-#             return queryset.filter(favorites__user=self.request.user)
-#         return queryset
-#
-#     def filter_is_in_shopping_cart(self, queryset, name, value):
-#         if value and self.request.user.is_authenticated:
-#             return queryset.filter(shopping_list__user=self.request.user)
-#         return
-
-
-class RecipeFilter(rest_framework.FilterSet):
-    is_favorited = rest_framework.BooleanFilter(method="get_favorited_filter")
-    is_in_shopping_cart = rest_framework.BooleanFilter(
-        method="get_shopping_cart_filter"
+class RecipeFilter(FilterSet):
+    tags = filters.ModelMultipleChoiceFilter(
+        field_name='tags__slug',
+        to_field_name='slug',
+        queryset=Tag.objects.all()
     )
-    author = rest_framework.NumberFilter(
-        field_name="author", lookup_expr="exact")
-    tags = rest_framework.ModelMultipleChoiceFilter(
-        field_name="tags__slug", to_field_name="slug",
-        queryset=Tag.objects.all())
+    is_favorited = filters.BooleanFilter(method='filter_is_favorited')
+    is_in_shopping_cart = filters.BooleanFilter(
+        method='filter_is_in_shopping_cart'
+    )
 
     class Meta:
         model = Recipe
-        fields = ("author", "tags", "is_favorited", "is_in_shopping_cart")
+        fields = ('tags', 'author', 'is_favorited', 'is_in_shopping_cart')
 
-    def get_favorited_filter(self, queryset, i, value):
+    def _get_queryset(self, queryset, name, value, model):
         if value:
-            return queryset.prefetch_related("favorite").filter(
-                favorite__user=self.request.user
-            )
+            return queryset.filter(**{FILTER_USER[model]: self.request.user})
         return queryset
 
-    def get_shopping_cart_filter(self, queryset, i, value):
-        if value:
-            return queryset.prefetch_related("carts").filter(
-                carts__user=self.request.user
-            )
-        return queryset
+    def filter_is_favorited(self, queryset, name, value):
+        return self._get_queryset(queryset, name, value, 'favorites')
+
+    def filter_is_in_shopping_cart(self, queryset, name, value):
+        return self._get_queryset(queryset, name, value, 'shopping_cart')
