1f46053cb6ab4c058d78bf747c442583f7dd0514
Kirill Svitsov <user@MacBook-Air-2.Home>
2023-10-20 21:42:47 +0100

    Test ingredient without filtr

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -1,14 +1,14 @@
 from django_filters.rest_framework import FilterSet, filters
 
-from recipes.models import Ingredient, Recipe, Tag
+from recipes.models import Recipe, Tag
 
 
-class IngredientFilter(FilterSet):
-    name = filters.CharFilter(lookup_expr='istartswith')
-
-    class Meta:
-        model = Ingredient
-        fields = ('name',)
+# class IngredientFilter(FilterSet):
+#     name = filters.CharFilter(lookup_expr='istartswith')
+#
+#     class Meta:
+#         model = Ingredient
+#         fields = ('name',)
 
 
 class RecipeFilter(FilterSet):
