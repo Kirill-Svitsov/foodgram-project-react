cf3ee2c12c3549a14b7d32eb5aa11b6c270fe042
Kirill Svitsov <user@MacBook-Air-2.Home>
2023-10-20 18:01:34 +0100

    Added user.is_anonymous

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -206,7 +206,8 @@ class RecipeSerializer(serializers.ModelSerializer):
 
     def get_is_in_shopping_cart(self, recipe):
         user = self.context.get('request').user
-        return recipe.shopping_list.filter(user=user).exists()
+        return (user.is_anonymous
+                and recipe.shopping_list.filter(user=user).exists())
 
 
 class ShoppingListSerializer(serializers.ModelSerializer):
