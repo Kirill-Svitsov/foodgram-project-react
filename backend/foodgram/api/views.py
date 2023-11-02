7f4702fc5d578c2a8da0bb0bd495d7895262acbb
Kirill Svitsov <user@MacBook-Air-2.Home>
2023-11-02 15:40:16 +0000

    Fix

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -4,7 +4,6 @@ from django_filters import rest_framework as django_filters
 from djoser.views import UserViewSet as DjoserUserViewSet
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
-from rest_framework import serializers
 from rest_framework.filters import SearchFilter
 from rest_framework.permissions import (
     AllowAny,
